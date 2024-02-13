ce684070d7bf15edbf5966e8995366b3a007a702
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2024-02-13 12:47:23 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    kotlin("jvm") version "1.9.20"
+    id("org.jetbrains.kotlinx.benchmark") version "0.4.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.4.10")
+}
