fda092c7321ecbee866ecf47d2aade01e4a8f257
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2024-03-07 12:04:45 +0200

    fixup! Add GitHub Actions workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,27 @@ dependencies {
 }
 
 benchmark {
+    configurations {
+        val iterate by creating {
+            include("iterateEntry")
+            include("iterateKeyValue")
+            include("iterateAndSetValue")
+            exclude("PolymorphicMapsBenchmark")
+        }
+        val storeEntries by creating {
+            include("storeEntries")
+            exclude("PolymorphicMapsBenchmark")
+        }
+        val polymorphicIterate by creating {
+            include("PolymorphicMapsBenchmark.iterateEntry")
+            include("PolymorphicMapsBenchmark.iterateKeyValue")
+            include("PolymorphicMapsBenchmark.iterateAndSetValue")
+        }
+        val polymorphicStoreEntries by creating {
+            include("PolymorphicMapsBenchmark.storeEntries")
+        }
+    }
+
     targets {
         register("main")
     }
